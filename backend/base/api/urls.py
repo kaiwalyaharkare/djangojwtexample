d30413ffb036ba6b1a22445d16db15c622978c6e
kaiwalyaharkare <kaiwalyaharkare@gmail.com>
2022-05-20 22:48:17 +0530

    Login Logout works

---
+++ backend/base/api/urls.py
@@ -0,0 +1,11 @@
+from . import view
+from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('', view.getRoutes),
+    path('token/', view.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
